8b897deb567fa55007c2bc860198666b63dd2f63
Junmark Chi <chijunmark97@gmail.com>
2022-11-11 21:53:17 +0800

    Add nav-link click animations

--- src/App.js
+++ src/App.js
@@ -7,11 +7,12 @@ function App() {
   const introTitle = useRef(null);
   const introContainer = useRef(null);
   const projectRef = useRef([]);
+  const navRef = useRef([]);
   const nav = useRef(null);
   const body = useRef(null);
   const projectContainer = useRef(null);
   const scrollTracker = useRef(null);
-  // const cursor = useRef(null);
+
   const [projects, setProjects] = useState([
     { id: "1", class: "project project1 grid-column-span-2", selected: false },
     { id: "2", class: "project project2", selected: false },
@@ -23,6 +24,13 @@ function App() {
     { id: "8", class: "project project8 grid-column-span-2", selected: false },
   ]);
 
+  const [navs, setNavs] = useState([
+    "Projects",
+    "Education",
+    "Contact",
+    "Resume",
+  ]);
+
   useEffect(() => {
     setTimeout(() => {
       introName.current.style.transform = "translateY(-30px)";
@@ -32,28 +40,11 @@ function App() {
         introContainer.current.style.scale = "0.8";
         introTitle.current.style.color = "gray";
         nav.current.style.opacity = "1";
-
-        // setTimeout(() => {
-        //   projectContainer.current.style.opacity = "1";
-        // }, 800);
       }, 1800);
     }, 1500);
   }, []);
 
-  // const mouseLeaveHandler = () => {
-  //   cursor.current.style.opacity = "0";
-  // };
-
-  // const mouseEnterHandler = () => {
-  //   cursor.current.style.opacity = "1";
-  // };
-
-  const moustEnterBody = (e) => {
-    // cursor.current.style.scale = "1.5";
-  };
-
-  const handleOnMouseMove = (e) => {
-    // const { currentTarget: target } = e;
+  const handleOnMouseMoveProject = (e) => {
     projectRef.current.map((project) => {
       const rect = project.getBoundingClientRect(),
         x = e.clientX - rect.left,
@@ -64,14 +55,13 @@ function App() {
     });
   };
 
+  const handleOnClickNav = (e) => {
+    nav.current.dataset.activeIndex = e.target.id;
+    console.log(e.target.id);
+  };
+
   return (
-    <div
-      id="app"
-      // onMouseLeave={mouseLeaveHandler}
-      // onMouseEnter={mouseEnterHandler}
-      ref={app}
-    >
-      {/* <i className="fa-solid fa-arrow-pointer" ref={cursor}></i> */}
+    <div id="app" ref={app}>
       <div className="intro-container" ref={introContainer}>
         <div className="intro-name" ref={introName}>
           JUNMARK CHI
@@ -80,11 +70,11 @@ function App() {
           WEB DEVELOPER
         </div>
       </div>
-      <div className="body" ref={body} onMouseEnter={moustEnterBody}>
+      <div className="body" ref={body}>
         <div
           className="project-container"
           ref={projectContainer}
-          onMouseMove={(e) => handleOnMouseMove(e)}
+          onMouseMove={(e) => handleOnMouseMoveProject(e)}
         >
           {projects.map((project, index) => {
             return (
@@ -92,30 +82,31 @@ function App() {
                 data-id={project.id}
                 className={project.class}
                 key={index}
-                ref={e => projectRef.current[index] = e}
+                ref={(e) => (projectRef.current[index] = e)}
               >
                 <div className="project-border"></div>
                 <div className="project-content"></div>
               </div>
             );
           })}
-          {/* <div className="project project1 grid-column-span-2"></div>
-          <div className="project project2"></div>
-          <div className="project project3"></div>
-          <div className="project project4 grid-column-span-2"></div>
-          <div className="project project5"></div>
-          <div className="project project6"></div>
-          <div className="project project7"></div>
-          <div className="project project8 grid-column-span-2"></div> */}
         </div>
       </div>
       <div className="scroll-tracker" ref={scrollTracker}></div>
       <div className="nav">
         <div className="nav-container" ref={nav}>
-          <a className="nav-link">Projects</a>
-          <a className="nav-link">Education</a>
-          <a className="nav-link">Contact</a>
-          <a className="nav-link">Resume</a>
+          {navs.map((nav, index) => {
+            return (
+              <a
+                id={index}
+                key={index}
+                className="nav-link"
+                onClick={(e) => handleOnClickNav(e)}
+                ref={(e) => (navRef[index] = e)}
+              >
+                {nav}
+              </a>
+            );
+          })}
           <div className="hover"></div>
         </div>
       </div>
