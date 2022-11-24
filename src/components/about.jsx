1a6088e0bdb8ebff2426de7ca21aaff4481be889
Junmark Chi <chijunmark97@gmail.com>
2022-11-24 23:29:36 +0800

    Update files

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -1,27 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/about.scss";
 
-function about() {
+function About() {
+  const [tech, setTech] = useState([
+    { src: "technologies/bootstrap.png" },
+    { src: "technologies/canva.png" },
+    { src: "technologies/css.png" },
+    { src: "technologies/git.png" },
+    { src: "technologies/github.png" },
+    { src: "technologies/html.png" },
+    { src: "technologies/insomnia.png" },
+    { src: "technologies/javascript.png" },
+    { src: "technologies/mysql.png" },
+    { src: "technologies/netlify.png" },
+    { src: "technologies/node.png" },
+    { src: "technologies/postgresql.png" },
+    { src: "technologies/postman.png" },
+    { src: "technologies/rails.png" },
+    { src: "technologies/react.png" },
+    { src: "technologies/ruby.png" },
+    { src: "technologies/sass.png" },
+    { src: "technologies/tailwind.png" },
+    { src: "technologies/typescript.png" },
+    { src: "technologies/vscode.png" },
+  ]);
+
   return (
     <div className="about-container">
       <h3>
         I'm<span>Junmark !</span>
       </h3>
       <h3>
-        Creative and Responsible Web Developer based in Molave, Philippines. I help companies solve problems and develop websites with
-        clean UI/UX.
+        Creative and Responsible Web Developer based in Philippines. I help
+        clients develop websites from the ground up.
       </h3>
-      <p>
-        {/* My current techstack involves working with ReactJS and Rails. */}
-      </p>
       <h2 className="technologies">Technologies</h2>
       <div className="technologies-logo">
-        <i className="fa-brands fa-square-js"></i>
-        <i className="fa-brands fa-react"></i>
-        <i className="fa-brands fa-html5"></i>
-        <i className="fa-brands fa-css3-alt"></i>
-        <i className="fa-brands fa-npm"></i>
-        <i className="fa-brands fa-github"></i>
+        <section className="carousel-slider">
+          {tech.map((t, idx) => {
+            return <img src={t.src} key={idx} />;
+          })}
+        </section>
       </div>
       <h2 className="educations">Education</h2>
       <p>Avion School</p>
@@ -30,4 +49,4 @@ function about() {
   );
 }
 
-export default about;
+export default About;
--- src/components/resume.jsx
+++ src/components/resume.jsx
@@ -10,6 +10,7 @@ function resume() {
       </a>
       <h6>OR</h6>
       <a
+        className="new-tab"
         href="https://docs.google.com/document/d/e/2PACX-1vQh887q6qwAvGJvgE6eFVcD5vTMZOSB64uEIAK-wH5ecvQImyhkT2bm-aRju6IsfysQYKv42SCee6pi/pub"
         target="_blank"
       >
